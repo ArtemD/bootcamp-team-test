79eeed2290a75a2ce3856c9975c088780658d021
Artem D <artem@daniliants.com>
2021-10-15 11:43:17 +0300

    test

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php print('Yo!'); ?>
 <?php include_once("src/index.html"); ?>
 <!DOCTYPE html>
@@ -8,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>testing</title>
-    <?php print('Yo-yo-yo!'); ?>
 </head>
 <body>
     
