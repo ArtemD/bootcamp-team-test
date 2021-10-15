18c5c606175003db434d98942696ca06a0276a52
petehuttunen <petri_huttunen84@hotmail.com>
2021-10-15 11:46:50 +0300

    removed useless lines

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php print('Yo!'); ?>
 <?php include_once("src/index.html"); ?>
 <!DOCTYPE html>
@@ -11,17 +9,11 @@
     <title>testing</title>
 </head>
 <body>
-    
+
 </body>
 </html>
-=======
-=======
+
 <?php print('Yo-yo-yo!'); ?>
->>>>>>> 0cb0342f632ae47d0fdc71484af69cfb182fcda2
+
 <?php include_once("src/index.html"); ?>
 <?php echo ("eehele"); ?>
-<<<<<<< HEAD
-
->>>>>>> 0d83aa7a7cb6955f78ec2d1ccf4c4e8767930cd1
-=======
->>>>>>> 0cb0342f632ae47d0fdc71484af69cfb182fcda2
